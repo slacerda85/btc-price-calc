f33337ae810a6dadc71be21d8cccef7b29d82e6d
stephani.lacerda <stephani.lacerda@sumaq.com.br>
2024-09-22 08:00:39 -0300

    bg opacity and color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { BitcoinCalculator } from "@/components/bitcoin-calculator";
 export default function Home() {
   return (
     <div className="bg-[url('/btc-bg.jpg')] bg-cover bg-center">
-    <div className="min-h-screen p-8 flex items-center justify-items-center backdrop-blur-sm">
+    <div className="min-h-screen p-8 flex items-center justify-items-center backdrop-blur-sm bg-black/30">
       <BitcoinCalculator />
     </div>
     </div>
